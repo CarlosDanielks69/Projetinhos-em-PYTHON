84a1d9bd79c46272a987d2d2db3a96cf494231c5
CarlosDanielks69 <92346886+CarlosDanielks69@users.noreply.github.com>
2023-10-18 12:19:28 -0300

    Create PEDRA, PAPEL e TESOURA em python.py
    joguinho simples feito em python, fiz para praticar IF, ELSE e ELIF,  junto com logica de programação

---
+++ PEDRA, PAPEL e TESOURA em python.py
@@ -0,0 +1,18 @@
+import random
+
+a = input("Pedra Papel ou Tesoura? ").upper()
+
+lista = ["PEDRA", "PAPEL", "TESOURA"]
+
+computador = random.choice(lista)
+
+if a not in lista:
+    print("Escolha Pedra, Papel ou Tesoura.")
+elif a == computador:
+    print(f"Você: {a}\nComputador: {computador}\nDRAW")
+elif a == "PEDRA" and computador == "PEDRA" or \
+        a == "PAPEL" and computador == "PEDRA" or \
+        a == "TESOURA" and computador == "PAPEL":
+    print(f"Você: {a}\nComputador {computador}\nYOU WIN!")
+else:
+    print(f"Você: {a}\nComputador {computador}\nYOU LOSE!")
