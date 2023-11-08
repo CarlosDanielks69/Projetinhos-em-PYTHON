dbddb8471ed06ef20f43e53d97b0497dcbaca93a
CarlosDanielks69 <92346886+CarlosDanielks69@users.noreply.github.com>
2023-11-08 11:08:16 -0300

    Update PEDRA, PAPEL e TESOURA em python.py

--- PEDRA, PAPEL e TESOURA em python.py
+++ PEDRA, PAPEL e TESOURA em python.py
@@ -1,18 +1,29 @@
 import random
 
-a = input("Pedra Papel ou Tesoura? ").upper()
-
 lista = ["PEDRA", "PAPEL", "TESOURA"]
 
-computador = random.choice(lista)
+placar_1 = 0
+placar_2 = 0
 
-if a not in lista:
-    print("Escolha Pedra, Papel ou Tesoura.")
-elif a == computador:
-    print(f"Você: {a}\nComputador: {computador}\nDRAW")
-elif (a == "PEDRA" and computador == "TEROURA") or \
-        (a == "PAPEL" and computador == "PEDRA") or \
-        (a == "TESOURA" and computador == "PAPEL"):
-    print(f"Você: {a}\nComputador {computador}\nYOU WIN!")
-else:
-    print(f"Você: {a}\nComputador {computador}\nYOU LOSE!")
+while True:
+    a = input("Pedra Papel ou Tesoura? ").upper()
+
+    computador = random.choice(lista)
+
+    if a not in lista:
+        print("Escolha Pedra, Papel ou Tesoura.")
+    elif a == computador:
+        print(f"Você: {a}\nComputador: {computador}\nDRAW")
+        input("Pressione enter para continar...")
+    elif (a == "PEDRA" and computador == "TESOURA") or \
+            (a == "PAPEL" and computador == "PEDRA") or \
+            (a == "TESOURA" and computador == "PAPEL"):
+        print(f"Você: {a}\nComputador {computador}\nYOU WIN!")
+        placar_1 = placar_1 + 1
+        print(f"\nO Placar está como {placar_1} para jogador e {placar_2} para maquina.")
+        input("Pressione enter para continar...")
+    else:
+        print(f"Você: {a}\nComputador {computador}\nYOU LOSE!")
+        placar_2 = placar_2 + 1
+        print(f"\nO Placar está como {placar_1} para jogador e {placar_2} para maquina.")
+        input("Pressione enter para continar...")
